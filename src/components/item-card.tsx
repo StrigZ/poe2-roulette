49738dfa6e3447cb33aad56080de1f8f02815bbe
strigz <mirgasan200@gmail.com>
2026-07-25 05:53:35 +0300

    Update prop names; Handle missing imageSrc

--- src/components/item-card.tsx
+++ src/components/item-card.tsx
@@ -1,13 +1,20 @@
+import { ImageOff } from "lucide-react";
 import type { Skill } from "../types";
 
-export function ItemCard({ name, src }: Skill) {
+export function ItemCard({ name, imageSrc }: Skill) {
   return (
     <div className="flex flex-col items-center gap-5">
-      <img
-        src={src}
-        className="aspect-square h-20 object-cover rounded-sm border-white"
-        alt={`иконка ${name}`}
-      />
+      {imageSrc ? (
+        <img
+          src={imageSrc}
+          className="aspect-square h-20 object-cover rounded-sm border-white"
+          alt={`иконка ${name}`}
+        />
+      ) : (
+        <div className="border-white border p-2 rounded">
+          <ImageOff size={48} className="stroke-white" />
+        </div>
+      )}
       <p className="font-bold text-lg text-white tracking-wide uppercase ">
         {name}
       </p>
