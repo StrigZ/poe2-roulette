29402beeb6cf4f9c7e5f403e6c9f783b5f45fe6e
strigz <mirgasan200@gmail.com>
2026-07-25 05:45:52 +0300

    Add new game to useGetRandom hook

--- src/hooks/use-get-random.ts
+++ src/hooks/use-get-random.ts
@@ -1,8 +1,26 @@
-import skills from "../assets/poe2_skills.json";
-import ascendancies from "../assets/poe2_ascendancies.json";
+import poe2Skills from "../assets/poe2_skills.json";
+import poe1Skills from "../assets/poe1_skills.json";
+import poe2Ascendancies from "../assets/poe2_ascendancies.json";
+import poe1Ascendancies from "../assets/poe2_ascendancies.json";
 import { getRandom } from "../utils/get-random";
+import type { Game } from "../types";
 
-export function useGetRandom() {
+function getData(game: Game) {
+  switch (game) {
+    case "poe1": {
+      return { skills: poe1Skills, ascendancies: poe1Ascendancies };
+    }
+    case "poe2":
+      return { skills: poe2Skills, ascendancies: poe2Ascendancies };
+
+    default:
+      throw new Error("getData: Wrong game");
+  }
+}
+
+export function useGetRandom({ game }: { game: Game }) {
+  const { skills, ascendancies } = getData(game);
+
   return {
     getRandomSkill: () => getRandom(skills),
     getRandomAscendancy: () => getRandom(ascendancies),
